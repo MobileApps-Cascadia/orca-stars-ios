6c38baaf93011b33d30f77e678a2a0a119c318dc
James Hayes <jchayes86@gmail.com>
2019-08-18 13:12:47 -0700

    Initial Commit

---
+++ orcastars/orcastars/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  orcastars
+//
+//  Created by Student Account on 8/18/19.
+//  Copyright © 2019 James Hayes. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
