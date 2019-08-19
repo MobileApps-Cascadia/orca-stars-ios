84c9d348a7f324b8ef972955bb000b6f4b1fbd2f
James Hayes <jchayes86@gmail.com>
2019-08-18 19:59:07 -0700

    added bg-gradient/menu button/search box

--- orcastars/orcastars/HomeViewController.swift
+++ orcastars/orcastars/HomeViewController.swift
@@ -9,12 +9,39 @@
 import UIKit
 
 class HomeViewController: UIViewController {
-
+    
+    @IBOutlet weak var searchBar: UITextField!
+    @IBOutlet weak var pageControl: UIPageControl!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        searchBar.clipsToBounds = true
+        searchBar.layer.cornerRadius = 20
+        
+        
+        pageControl.layer.shadowColor = UIColor.black.cgColor
+        pageControl.layer.shadowPath = UIBezierPath(rect: pageControl.bounds).cgPath
+        pageControl.layer.shouldRasterize = true
+        
+        let width: CGFloat = 375
+        let height: CGFloat = 37
+        let shadowSize: CGFloat = 10
+        let contactRect = CGRect(x: 0, y: height - (shadowSize * 0.4), width: width, height: shadowSize)
+        
+        pageControl.layer.shadowPath = UIBezierPath(ovalIn: contactRect).cgPath
+        pageControl.layer.shadowRadius = 5
+        pageControl.layer.shadowOpacity = 0.25
     }
 
-
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
 }
-
