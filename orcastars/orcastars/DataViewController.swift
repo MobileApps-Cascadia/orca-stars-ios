9cb33899f9a7b03cfe3e25b48b0f8114c139e1ea
James Hayes <jchayes86@gmail.com>
2019-08-19 16:00:52 -0700

    fixed home screen ui constraint. added files for pageviewcontroller

---
+++ orcastars/orcastars/DataViewController.swift
@@ -0,0 +1,23 @@
+//
+//  DataViewController.swift
+//  orcastars
+//
+//  Created by James Hayes on 8/19/19.
+//  Copyright © 2019 James Hayes. All rights reserved.
+//
+
+import UIKit
+
+class DataViewController: UIViewController {
+
+    @IBOutlet weak var displayLabel: UILabel!
+    var displayText: String?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        displayLabel.text = displayText
+        // Do any additional setup after loading the view.
+    }
+
+}
