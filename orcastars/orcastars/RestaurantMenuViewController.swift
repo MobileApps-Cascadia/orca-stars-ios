dd0507237c28042353bd33cff9285dc27d8f60a5
Charles Sy <charles.m.sy@hotmail.com>
2020-04-30 21:54:01 -0700

    More minor updates

--- orcastars/orcastars/RestaurantMenuViewController.swift
+++ orcastars/orcastars/RestaurantMenuViewController.swift
@@ -8,11 +8,10 @@
 
 import UIKit
 
-class RestaurantMenuViewController: UIPageViewController {
-
+class RestaurantMenuViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-    
+
 }
