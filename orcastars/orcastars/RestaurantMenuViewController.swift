ac6c14568c22e8ead890aa929581cfbea905442c
Charles Sy <charles.m.sy@hotmail.com>
2020-04-30 21:18:17 -0700

    minor changes

---
+++ orcastars/orcastars/RestaurantMenuViewController.swift
@@ -0,0 +1,18 @@
+//
+//  RestaurantMenuViewController.swift
+//  orcastars
+//
+//  Created by Mac Computer on 4/30/20.
+//  Copyright © 2020 James Hayes. All rights reserved.
+//
+
+import UIKit
+
+class RestaurantMenuViewController: UIPageViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+}
