9f0177388b38b6772a6c51cac806d3634bd5d19c
Charles Sy <charles.m.sy@hotmail.com>
2020-06-10 22:35:35 -0700

    More menu displays categories and table view has background color.

--- orcastars/orcastars/Views/MoreMenuViewController.swift
+++ orcastars/orcastars/Views/MoreMenuViewController.swift
@@ -9,15 +9,40 @@
 import Foundation
 import UIKit
 
-class MoreMenuViewController: UIViewController {
+class MoreMenuViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var myTableView: UITableView!
     
     private var cornerRadius: CGFloat = 16  // corner radius for most views
     let customLightBlue = UIColor(red: 190/255, green: 209/255, blue: 224/255, alpha: 1)
     let customBlue = UIColor(red: 5/255, green: 129/255, blue: 198/255, alpha: 1)
     
+    let categoriesArray = ["Restaurants", "Bar & NightLife", "Shopping", "Health & Medical", "Home Maintenence", "Professional Service", "Auto", "Hotel & Travel", "Pets", "Financial Service", "Mass Media", "Education"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        let gradientlayer = view.setGradientBackground(colorOne: customBlue, colorTwo: customLightBlue)
-        view.layer.insertSublayer(gradientlayer, at: 0)
+        let gradientLayer = view.setGradientBackground(colorOne: customBlue, colorTwo: customLightBlue)
+        let bgView = UIView.init(frame: self.myTableView.frame)
+        bgView.layer.insertSublayer(gradientLayer, at: 0)
+        self.myTableView.backgroundView = bgView
+        myTableView.dataSource = self
+        myTableView.delegate = self
     }
+    
+    //self.tableView.frame
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return categoriesArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MyCell", for: indexPath)
+        cell.textLabel!.text = categoriesArray[indexPath.row]
+        return cell
+    }
+    
 }
+
